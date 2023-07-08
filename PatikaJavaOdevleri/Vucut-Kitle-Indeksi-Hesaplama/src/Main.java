d845902643493cf7a73d5c9867b1a46c217e5436
Hava Isr <hava.isrplv@gmail.com>
2023-07-08 20:37:48 +0300

    Initial Commit

--- PatikaJavaOdevleri/Vucut-Kitle-Indeksi-Hesaplama/src/Main.java
+++ PatikaJavaOdevleri/Vucut-Kitle-Indeksi-Hesaplama/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int kilo;
+        double boy,indeks;
+
+        System.out.println("Vücut Kitle İndeksi için");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen kilonuzu (kg) giriniz:");
+        kilo = input.nextInt();
+
+        System.out.println("Lütfen boyunuzu (m) giriniz:");
+        boy = input.nextDouble();
+
+        indeks = kilo / (boy * boy);
+        System.out.println("Vücut indeksiniz: " + indeks);
+
     }
 }
